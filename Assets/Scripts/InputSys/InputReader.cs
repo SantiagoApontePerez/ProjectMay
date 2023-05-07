ab49dbd5758f2d7bd691e3f1a56a4ed452399fc1
SantiagoApontePerez <101404uk@saeinstitute.edu>
2023-05-07 18:48:04 +0100

    Implement of basic input reader

---
+++ Assets/Scripts/InputSys/InputReader.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : ThisPlay.IPlayerActions {
+
+    #region Reader Setup
+    private ThisPlay _controls;
+
+    private void OnEnable() {
+        SetUpControlls();
+        _controls.Player.Enable();
+    }
+
+    private void OnDisable() {
+        _controls.Player.Enable();
+    }
+
+    private void SetUpControlls() {
+        _controls = (_controls is null) ? new ThisPlay() : _controls;
+        _controls.Player.SetCallbacks(this);
+    }
+    #endregion
+
+    #region IPlayerActions *In Game*
+
+    public event Action LClick = delegate { };
+    public event Action RClick = delegate { };
+    public event Action PJump  = delegate { };
+    public Vector2 MovAxis { get; private set; }
+
+    public void OnJump(InputAction.CallbackContext context) {
+        PJump?.Invoke();
+    }
+
+    public void OnMouseLeft(InputAction.CallbackContext context) {
+        LClick?.Invoke();
+    }
+
+    public void OnMouseRight(InputAction.CallbackContext context) {
+        RClick?.Invoke();
+    }
+
+    public void OnMove(InputAction.CallbackContext context) {
+        this.MovAxis = context.ReadValue<Vector2>();
+    }
+    #endregion
+}
